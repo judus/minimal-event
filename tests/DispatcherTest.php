ca9987a2eea9eb20957eea420b1b780439dec284
Julien Duseyau <jduseyau@previon.ch>
2019-02-17 12:08:25 +0100

    Updates to phpunit 8

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -22,7 +22,7 @@ class DispatcherTest extends TestCase
     /**
      *
      */
-    public function setUp()
+    public function setUp():void
     {
         $this->dispatcher = new Dispatcher;
         $this->subscriber = new SubscriberStub;
